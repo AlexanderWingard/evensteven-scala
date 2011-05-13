5cd8e52b0845c8cd7a22aa0a7592a6f9537e5c78
Alexander Wingård <alexander.wingard@gmail.com>
2011-05-13 02:27:11 +0200

    Parsing Bills with all three kinds of split, names are not trimmed

--- src/main/scala/evensteven/Main.scala
+++ src/main/scala/evensteven/Main.scala
@@ -1,7 +1,40 @@
 package evensteven
 
+import scala.io.Source
+import util.matching.Regex
+
 object Evensteven {
-  def main(args: Array[String]) {
+  val billRegexp = new Regex("""\*\s*(\S+)\s*""")
+  val splitRegexp = new Regex("""\s*([\+-]*)(\d+)\s*([a-zA-Z,\s]+)(.*)""")
+  def main(args: Array[String]) = {
     println("Hello EvenSteven")
   }
+
+  def parse(source : Source) = {
+    source.getLines().foldLeft(List() : List[Bill]) { (bills, line) =>
+      line match {
+	case billRegexp(name) =>
+	  Bill(name) :: bills
+	case splitRegexp(kind, amount, splitters, comment) =>
+	  val split = Split(splitters.split(",").toList, amount.toInt)
+	  val bill :: tail = bills
+	  kind match {
+	    case "" =>
+	      bill.copy(split = Some(split)) :: tail
+	    case "-" =>
+	      bill.copy(subSplits = split :: bill.subSplits) :: tail
+	    case "+" =>
+	      bill.copy(payments = split :: bill.payments) :: tail
+	  }
+	case other =>
+	  bills
+      }
+    }
+  }
 }
+
+case class Bill(name : String, split : Option[Split] = None, subSplits : List[Split] = List(), payments : List[Split] = List())
+
+case class Split(splitters : List[String], amount : Number)
+
+case class Transfer(from : String, to : String, amount : Number)
--- src/test/scala/evensteven/test/EvenTest.scala
+++ src/test/scala/evensteven/test/EvenTest.scala
@@ -3,8 +3,37 @@ package evensteven.test
 import org.scalatest.Spec
 import org.scalatest.matchers.ShouldMatchers
 
+import scala.io.Source
+
+import evensteven._
+
 class EvenTest extends Spec with ShouldMatchers {
+  val example =
+"""
+* Matkasse
+  // Lite Mat
+  100 Alex, Lasse, Stefan, Chrisitan
+  -20 Alex, Lasse // Två glassar
+  +100 Lasse
+""";
+
   describe ("EvenSteven") {
 
+    val result = Evensteven.parse(Source.fromString(example))
+
+    it("should parse one bill") {
+      result should have length (1)
+    }
+    it("should parse title correctly") {
+      val bill = result.head
+      bill.name should equal ("Matkasse")
+    }
+    it("should parse correct number of splitters") {
+      val bill = result.head
+
+      bill.split.get.splitters should have length (4)
+      bill.subSplits.head.splitters should have size (2)
+      bill.payments.head.splitters should have size (1)
+    }
   }
 }
