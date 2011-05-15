71b70720d257b8505ff59a7f68da9e9bd14edec5
Alexander Wingård <alexander.wingard@gmail.com>
2011-05-15 21:59:07 +0200

    Add transfer regexp

--- src/main/scala/evensteven/Main.scala
+++ src/main/scala/evensteven/Main.scala
@@ -6,6 +6,8 @@ import util.matching.Regex
 object Evensteven {
   val billRegexp = new Regex("""\*\s*(\S+)\s*""")
   val splitRegexp = new Regex("""\s*([\+-]*)(\d+)\s*([a-zA-Z,\s]+)(.*)""")
+  val transferRegexp = new Regex("""\s*>(\d+)\s+([a-zA-Z])[\s,]+([a-zA-Z])(.*)""")
+
   def main(args: Array[String]) = {
     println("Hello EvenSteven")
   }
